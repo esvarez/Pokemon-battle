e3db0017f0704d60e4418832711755db48ce7c24
Erick Suarez <ericksuarezdev@gmail.com>
2020-07-27 10:43:18 -0500

    Adding integration test

--- src/test/java/dev/ericksuarez/pokemon/battle/controller/BattleControllerTest.java
+++ src/test/java/dev/ericksuarez/pokemon/battle/controller/BattleControllerTest.java
@@ -14,25 +14,51 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-/*
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
- */
 public class BattleControllerTest {
-    /*
+
     @Autowired
     private MockMvc mockMvc;
-
 
     @Test
     public void battlePokemon_grassDealDoubleDamageToWater_returnAnalysis() throws Exception {
         mockMvc.perform(get("/api/battle/{idPokemon}/vs/{idPokemon2}", 1, 8))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.battle", is("bulbasaur vs wartortle")))
-                .andExpect(jsonPath("$double_damage", is("bulbasaur deal double damage to wartortle")));
+                .andExpect(jsonPath("$.double_damage", is("bulbasaur deal double damage to wartortle")));
     }
-     */
 
+    @Test
+    public void battlePokemon_fireReceiveHalfDamageFromIce_returnAnalysis() throws Exception {
+        mockMvc.perform(get("/api/battle/{idPokemon}/vs/{idPokemon2}", 6, "jynx"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.battle", is("charizard vs jynx")))
+                .andExpect(jsonPath("$.half_damage", is("charizard receive half damage from jynx")));
+    }
+
+    @Test
+    public void compare_leaveDefaultValues_returnMovesPaged() throws Exception {
+        mockMvc.perform(get("/api/compare?pokemons={idPokemon}", "1,2,3"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.lang", is("en")))
+                .andExpect(jsonPath("$.page", is(1)))
+                .andExpect(jsonPath("$.moves", is(10)));
+    }
+
+    @Test
+    public void compare_translateMoves_returnMovesPaged() throws Exception {
+        mockMvc.perform(get("/api/compare?pokemons={idPokemon}&lang={lang}&page={page}&limit={limit}"
+                , "1,2,3", "es", 2, 5))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.lang", is("es")))
+                .andExpect(jsonPath("$.page", is(2)))
+                .andExpect(jsonPath("$.moves", is(5)));
+    }
 }
