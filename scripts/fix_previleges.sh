3f960cfc401cf3b4d1d145e165c4c85315581348
Erick Suarez <ericksuarezdev@gmail.com>
2020-07-28 01:49:09 -0500

    more routes

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x /tmp/pokemon-battle-0.0.1-SNAPSHOT.jar
-chmod +x /scripts/start_server.sh
-chmod +x /scripts/stop_server.sh
+chmod +x /tmp/deploy/pokemon-battle-0.0.1-SNAPSHOT.jar
+chmod +x /tmp/deploy/scripts/start_server.sh
+chmod +x /tmp/deploy/scripts/stop_server.sh
 
